4a799717f8b1248d88e2726a60c1ee128d4fc9e7
Bolotov Alexander <distalker1@gmail.com>
2022-01-17 13:52:50 +0300

    pom fix

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -6,6 +6,7 @@ import javax.persistence.Table;
 
 @Table
 public class User {
+
     @Id
     private Long id;
 
@@ -18,9 +19,7 @@ public class User {
     @Column
     private Byte age;
 
-    public User() {
-
-    }
+    public User() { }
 
     public User(String name, String lastName, Byte age) {
         this.name = name;
@@ -58,5 +57,13 @@ public class User {
 
     public void setAge(Byte age) {
         this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("User {id = '%s'," +
+                " name = '%s'," +
+                " lastName = '%s'," +
+                " age = '%d'}", getId(), getName(), getLastName(), getAge());
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,24 @@
 package jm.task.core.jdbc.util;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/kamil_db";
+    public static final String DB_USERNAME = "root";
+    public static final String DB_PASSWORD = "root";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+            try {
+                connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+                System.out.println("Connected!");
+            } catch (SQLException e) {
+                e.printStackTrace();
+                System.out.println("Connection error!");
+            }
+        return connection;
+    }
 }
